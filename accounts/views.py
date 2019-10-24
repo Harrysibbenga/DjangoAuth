208980fc389fb15d9804ee43af7cc103ba6560fb
Harry Sibbenga <sibbengaharry@gmail.com>
2019-10-24 11:50:35 +0100

    Added a logout view and the index view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth
 
 # Create your views here.
+def index(request): 
+    # Return the index.html template
+    return render(request, 'index.html')
+
+def logout(request): 
+    # Logout the user
+    auth.logout(request)
+    return redirect(reverse('index'))
